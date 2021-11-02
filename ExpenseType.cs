ddc435fce02a12ce6eee6e4c5514f51395c6566b
Ivo286 <93599972+Ivo286@users.noreply.github.com>
2021-11-02 16:46:49 +0200

    Add files via upload

---
+++ ExpenseType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zad1_2._11._21.Models
+{
+    public class ExpenseType
+    {
+        int Id;
+        string ExpenseTypeName;
+    }
+}
---
+++ Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zad1_2._11._21.Models
+{
+    public class Item
+    {
+        int Id;
+        string ItemName;
+        string Borrower;
+        string Lender;
+    }
+}
